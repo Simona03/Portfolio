2b7518a8e1e1c7d2eacddc563b409af7e18ec4fe
Simona <ivanovasimona787@gmail.com>
2022-08-20 10:09:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Imports
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 2500;
+
+// Static Files
+app.use(express.static("tools"));
+
+// Set View's
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// Navigation
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+
+
+
+
+app.listen(port, () => console.info(`App listening on port ${port}`));
